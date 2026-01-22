c154b4c5ae0f1fc9a0bf5047566a892ef7f4c58c
Montaser Asad <masad@c1r2s12.42amman.com>
2026-01-22 17:08:03 +0300

    wiwi talk done UwU

--- mini_talk/client.c
+++ mini_talk/client.c
@@ -6,7 +6,7 @@
 /*   By: masad <masad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 18:26:33 by masad             #+#    #+#             */
-/*   Updated: 2026/01/17 19:14:00 by masad            ###   ########.fr       */
+/*   Updated: 2026/01/22 17:01:11 by masad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,14 @@
 #include <signal.h>
 #include <unistd.h>
 
+int		g_ack_received;
+
+void	ack(int sig)
+{
+	(void)sig;
+	g_ack_received = 1;
+}
+
 void	send_sig(int pid, const char c)
 {
 	int	i;
@@ -22,31 +30,50 @@ void	send_sig(int pid, const char c)
 	i = 7;
 	while (i >= 0)
 	{
+		g_ack_received = 0;
 		if ((c >> i) & 1)
 			kill(pid, SIGUSR1);
 		else
 			kill(pid, SIGUSR2);
-		usleep(420);
+		while (g_ack_received == 0)
+			;
 		i--;
 	}
 }
 
+int	check_pid(int pid)
+{
+	if (pid <= 0)
+		return (-1);
+	if (kill(pid, 0) == -1)
+		return (-1);
+	return (0);
+}
+
 int	main(int argc, const char *argv[])
 {
-	int			pid;
-	const char	*message;
-	int			i;
+	int					pid;
+	const char			*message;
+	struct sigaction	action;
 
-	i = -1;
 	if (argc != 3)
 	{
 		ft_printf("Usage: %s <pid> <message>\n", argv[0]);
 		exit(0);
 	}
 	pid = ft_atoi(argv[1]);
+	if (check_pid(pid) == -1)
+	{
+		ft_printf("Error: Invalid PID\n");
+		exit(0);
+	}
+	action.sa_handler = &ack;
+	action.sa_flags = 0;
+	sigemptyset(&action.sa_mask);
+	sigaction(SIGUSR1, &action, NULL);
 	message = argv[2];
-	while (message[++i])
-		send_sig(pid, message[i]);
+	while (*message)
+		send_sig(pid, *message++);
 	send_sig(pid, '\0');
 	return (0);
 }
--- mini_talk/server.c
+++ mini_talk/server.c
@@ -6,7 +6,7 @@
 /*   By: masad <masad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 14:42:36 by masad             #+#    #+#             */
-/*   Updated: 2026/01/17 19:10:43 by masad            ###   ########.fr       */
+/*   Updated: 2026/01/22 16:53:36 by masad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,30 @@
 #include "libft/libft.h"
 #include <signal.h>
 
-void	sighandler(int sig)
+static char	*g_message = NULL;
+
+void	cnct(char c)
+{
+	char	tmp[2];
+	char	*joined;
+
+	if (g_message == NULL)
+		g_message = ft_strdup("");
+	tmp[0] = c;
+	tmp[1] = '\0';
+	joined = ft_strjoin(g_message, tmp);
+	free(g_message);
+	g_message = joined;
+}
+
+void	sighandler(int sig, siginfo_t *info, void *context)
 {
 	static unsigned char	c;
 	static int				i;
+	int						pid;
 
+	pid = info->si_pid;
+	(void)context;
 	c <<= 1;
 	if (sig == SIGUSR1)
 		c |= 1;
@@ -26,12 +45,18 @@ void	sighandler(int sig)
 	if (i == 8)
 	{
 		if (c == '\0')
-			ft_printf("\n");
+		{
+			cnct('\n');
+			ft_printf("%s", g_message);
+			free(g_message);
+			g_message = NULL;
+		}
 		else
-			ft_printf("%c", c);
+			cnct(c);
 		i = 0;
 		c = 0;
 	}
+	kill(pid, SIGUSR1);
 }
 
 int	main(void)
@@ -39,9 +64,9 @@ int	main(void)
 	struct sigaction	action;
 
 	ft_printf("Server PID: %d\n", getpid());
-	action.sa_handler = sighandler;
+	action.sa_sigaction = &sighandler;
+	action.sa_flags = SA_SIGINFO;
 	sigemptyset(&action.sa_mask);
-	action.sa_flags = 0;
 	sigaction(SIGUSR1, &action, NULL);
 	sigaction(SIGUSR2, &action, NULL);
 	while (1)
